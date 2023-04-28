fafac6c1d0146699bce3b6a8eaa3e1182d282821
Christina2208 <christinalf22@gmail.com>
2023-04-27 21:28:02 -0400

    added variables

--- script.js
+++ script.js
@@ -10,10 +10,21 @@
 // have variables for total wins, total loses. increment variable by 1 and each condition appropriatly show those results to user
 //ask to play again, if play again start from beginning, otherwise end game and show the HTML
 
-var userChoice = window.prompt("Choose one: Rock, Paper or Scissors");
-alert("Choose One");
+var wins = 0
+var ties = 0
+var loses = 0
+
+var options = ["R", "P", "S"]
+
+var userChoice = window.prompt("Choose one: R, P or S");
 console.log(userChoice)
 
+if (!userChoice) {
+    alert("you need to type something in");
+    playGame();
+}
 
+userChouce = userChoice.toUpperCase();
 
-let x = Math.floor((Math.random() * 10));
+var index = Math.floor((Math.random()* options.length));
+var computerChoice = options[index];
